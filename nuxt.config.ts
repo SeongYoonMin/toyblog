8d49033b7a8ce69bd2e653591a8d1c8bec9a0f7c
SeongYoonMin <namo4862@naver.com>
2023-09-14 19:48:10 +0900

    Fix: add modules, runtimeconfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,11 @@ export default defineNuxtConfig({
     shim: false,
   },
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      secret: process.env.ADMIN_DATA,
+    },
+  },
   app: {
     head: {
       title: "Toy Blog",
@@ -11,20 +16,17 @@ export default defineNuxtConfig({
         {
           rel: "stylesheet",
           as: "style",
-          href: "https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css"
-        }
+          href: "https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css",
+        },
       ],
     },
   },
-  css: [
-    "~/assets/scss/reset.scss",
-    "~/assets/scss/main.scss",
-    "swiper/css",
-  ],
+  css: ["~/assets/scss/reset.scss", "~/assets/scss/main.scss", "swiper/css"],
+  modules: ['@pinia/nuxt'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
-    }
-  }
-})
+    },
+  },
+});
