4b11e5ab045a20a6837f639b60b34fbe59d01b1e
SeongYoonMin <namo4862@naver.com>
2023-09-15 20:17:40 +0900

    Fix: page style

--- pages/board/[id].vue
+++ pages/board/[id].vue
@@ -10,11 +10,15 @@ const { data } = await useApiFetch<IBoardItems>("/api/board/read", {
 
 <template>
   <section>
-    <div v-if="data">
-      <p>{{ data.title }}</p>
-      <p>{{ data.name }}</p>
-      <p>{{ data.date }}</p>
-      <p>{{ data.content }}</p>
+    <div v-if="data" class="data flex flex-col w-full items-start justify-center gap-10">
+      <div class="header flex items-start justify-between w-full">
+        <h1 class="title font-semibold text-2xl">{{ data.title }}</h1>
+        <div class="info flex items-end justify-center flex-col">
+          <p class="name">{{ data.name }}</p>
+          <p class="date">{{ data.date }}</p>
+        </div>
+      </div>
+      <div v-html="data.content" class="content"></div>
     </div>
     <div v-else>잘못된 아이디값 입니다.</div>
   </section>
