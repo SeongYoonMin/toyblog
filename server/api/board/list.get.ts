91a7887205fb79d2986e8eaf5ffd0997ed5ab7a2
SeongYoonMin <namo4862@naver.com>
2023-09-15 20:17:47 +0900

    Fix: types

--- server/api/board/list.get.ts
+++ server/api/board/list.get.ts
@@ -2,6 +2,18 @@ import { PrismaClient } from "@prisma/client";
 
 export default defineEventHandler((event) => {
   const prisma = new PrismaClient();
-  const list = prisma.board.findMany();
-  return list;
+  try {
+    const list = prisma.board.findMany({
+      orderBy: {
+        date: "desc",
+      },
+    });
+    return list;
+  } catch (error) {
+    throw createError({
+      statusCode: 502,
+      statusMessage:
+        "서버에 장애가 발생하였습니다. 잠시후 다시 시도해주시기 바랍니다.",
+    });
+  }
 });
