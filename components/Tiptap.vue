108c6fde2c2b6c469aef8da82df2158e3fe9c1ac
SeongYoonMin <namo4862@naver.com>
2023-09-27 18:50:26 +0900

    Fix: add bulletlist/ remove bubblelist

--- components/Tiptap.vue
+++ components/Tiptap.vue
@@ -2,7 +2,7 @@
 import { useEditor, EditorContent } from "@tiptap/vue-3";
 import StarterKit from "@tiptap/starter-kit";
 import Heading from "@tiptap/extension-heading";
-import BubbleMenu from "@tiptap/extension-bubble-menu";
+import BulletList from '@tiptap/extension-bullet-list'
 import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 import { common, createLowlight } from "lowlight";
 import css from "highlight.js/lib/languages/css";
@@ -21,16 +21,13 @@ lowlight.register("html", html);
 lowlight.register("js", js);
 lowlight.register("ts", ts);
 lowlight.register("css", css);
-const bubbleMenu = ref<HTMLElement | null>(null);
-bubbleMenu.value = document.querySelector(".bubble_menu");
 const editor = useEditor({
   content: props.modelValue,
   extensions: [
     StarterKit,
     Heading,
-    BubbleMenu.configure({
-      element: bubbleMenu.value,
-    }),
+    BulletList,
+    
     CodeBlockLowlight.configure({
       lowlight,
     }),
@@ -53,97 +50,78 @@ watch(
 </script>
 
 <template>
-  <section>
-    <div v-if="editor" class="flex items-center justify-start gap-2 flex-wrap">
-      <div class="bubble_menu"></div>
-      <button
-        @click="editor.chain().focus().toggleBold().run()"
-        :disabled="!editor.can().chain().focus().toggleBold().run()"
-        :class="{ 'is-active': editor.isActive('bold') }"
-      >
-        B
-      </button>
-      <button
-        @click="editor.chain().focus().toggleCodeBlock().run()"
-        :disabled="!editor.can().chain().focus().toggleCode().run()"
-        :class="{ 'is-active': editor.isActive('code') }"
-      >
-        > CODE
-      </button>
-      <button
-        @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
-      >
-        H1
-      </button>
-      <button
-        @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
-      >
-        H2
-      </button>
-      <button
-        @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
-      >
-        H3
-      </button>
-      <button
-        @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
-      >
-        H4
-      </button>
-      <button
-        @click="editor.chain().focus().toggleBulletList().run()"
-        :class="{ 'is-active': editor.isActive('bulletList') }"
-      >
-        bullet list
-      </button>
-      <button
-        @click="editor.chain().focus().toggleOrderedList().run()"
-        :class="{ 'is-active': editor.isActive('orderedList') }"
-      >
-        ordered list
-      </button>
-      <button
-        @click="editor.chain().focus().toggleCodeBlock().run()"
-        :class="{ 'is-active': editor.isActive('codeBlock') }"
-      >
-        code block
-      </button>
-      <button
-        @click="editor.chain().focus().toggleBlockquote().run()"
-        :class="{ 'is-active': editor.isActive('blockquote') }"
-      >
-        blockquote
-      </button>
-      <button @click="editor.chain().focus().setHorizontalRule().run()">
-        horizontal rule
-      </button>
-      <button @click="editor.chain().focus().setHardBreak().run()">
-        hard break
-      </button>
-      <button
-        @click="editor.chain().focus().undo().run()"
-        :disabled="!editor.can().chain().focus().undo().run()"
-      >
-        undo
-      </button>
-      <button
-        @click="editor.chain().focus().redo().run()"
-        :disabled="!editor.can().chain().focus().redo().run()"
-      >
-        redo
-      </button>
+  <section class="editor py-10">
+    <div v-if="editor" class="flex items-center justify-between flex-wrap pb-4">
+      <div class="font flex items-center justify-center gap-2">
+        <button
+          @click="editor.chain().focus().toggleBold().run()"
+          :disabled="!editor.can().chain().focus().toggleBold().run()"
+          :class="{ 'is-active': editor.isActive('bold') }"
+        >
+          <img src="~assets/images/icons/bold.png" alt="굵기" />
+        </button>
+        <button
+          @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+          :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+        >
+          <img src="~assets/images/icons/h-1.png" alt="H1" />
+        </button>
+        <button
+          @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+          :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
+        >
+          <img src="~assets/images/icons/h-2.png" alt="H2" />
+        </button>
+        <button
+          @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+          :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
+        >
+          <img src="~assets/images/icons/h-3.png" alt="H3" />
+        </button>
+        <button
+          @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
+          :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
+        >
+          <img src="~assets/images/icons/h-4.png" alt="H4" />
+        </button>
+        <button
+          @click="editor.chain().focus().toggleBulletList().run()"
+          :class="{ 'is-active': editor.isActive('bulletList') }"
+        >
+          <img src="~assets/images/icons/list-unordered.png" alt="bullet list" />
+        </button>
+        <button
+          @click="editor.chain().focus().toggleOrderedList().run()"
+          :class="{ 'is-active': editor.isActive('orderedList') }"
+        >
+          <img src="~assets/images/icons/list-ordered-2.png" alt="ordered list" />
+        </button>
+        <button
+          @click="editor.chain().focus().toggleCodeBlock().run()"
+          :disabled="!editor.can().chain().focus().toggleCode().run()"
+          :class="{ 'is-active': editor.isActive('code') }"
+        >
+          <img src="~assets/images/icons/code-view.png" alt="코드" />
+        </button>
+      </div>
+      <div class="history flex items-center justify-center gap-2">
+        <button
+          @click="editor.chain().focus().undo().run()"
+          :disabled="!editor.can().chain().focus().undo().run()"
+        >
+          <img src="~assets/images/icons/undo.png" alt="UNDO" />
+        </button>
+        <button
+          @click="editor.chain().focus().redo().run()"
+          :disabled="!editor.can().chain().focus().redo().run()"
+        >
+          <img src="~assets/images/icons/redo.png" alt="REDO" />
+        </button>
+      </div>
     </div>
     <editor-content :editor="editor" />
   </section>
 </template>
 
 <style lang="scss" scoped>
-button {
-  display: block;
-  margin-bottom: 8px;
-}
 </style>
