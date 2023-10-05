d756f1b54efa06a3095112ae8ff14eb7273bb086
Seongyoon <namo4862@gmail.com>
2023-10-06 00:56:22 +0900

    Fix: button type, active style

--- components/Tiptap.vue
+++ components/Tiptap.vue
@@ -2,7 +2,7 @@
 import { useEditor, EditorContent } from "@tiptap/vue-3";
 import StarterKit from "@tiptap/starter-kit";
 import Heading from "@tiptap/extension-heading";
-import BulletList from '@tiptap/extension-bullet-list'
+import BulletList from "@tiptap/extension-bullet-list";
 import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 import { common, createLowlight } from "lowlight";
 import css from "highlight.js/lib/languages/css";
@@ -27,7 +27,6 @@ const editor = useEditor({
     StarterKit,
     Heading,
     BulletList,
-    
     CodeBlockLowlight.configure({
       lowlight,
     }),
@@ -54,6 +53,7 @@ watch(
     <div v-if="editor" class="flex items-center justify-between flex-wrap pb-4">
       <div class="font flex items-center justify-center gap-2">
         <button
+          type="button"
           @click="editor.chain().focus().toggleBold().run()"
           :disabled="!editor.can().chain().focus().toggleBold().run()"
           :class="{ 'is-active': editor.isActive('bold') }"
@@ -61,42 +61,55 @@ watch(
           <img src="~assets/images/icons/bold.png" alt="굵기" />
         </button>
         <button
+          type="button"
           @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
           :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
         >
           <img src="~assets/images/icons/h-1.png" alt="H1" />
         </button>
         <button
+          type="button"
           @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
           :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
         >
           <img src="~assets/images/icons/h-2.png" alt="H2" />
         </button>
         <button
+          type="button"
           @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
           :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
         >
           <img src="~assets/images/icons/h-3.png" alt="H3" />
         </button>
         <button
+          type="button"
           @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
           :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
         >
           <img src="~assets/images/icons/h-4.png" alt="H4" />
         </button>
         <button
+          type="button"
           @click="editor.chain().focus().toggleBulletList().run()"
           :class="{ 'is-active': editor.isActive('bulletList') }"
         >
-          <img src="~assets/images/icons/list-unordered.png" alt="bullet list" />
+          <img
+            src="~assets/images/icons/list-unordered.png"
+            alt="bullet list"
+          />
         </button>
         <button
+          type="button"
           @click="editor.chain().focus().toggleOrderedList().run()"
           :class="{ 'is-active': editor.isActive('orderedList') }"
         >
-          <img src="~assets/images/icons/list-ordered-2.png" alt="ordered list" />
+          <img
+            src="~assets/images/icons/list-ordered-2.png"
+            alt="ordered list"
+          />
         </button>
         <button
+          type="button"
           @click="editor.chain().focus().toggleCodeBlock().run()"
           :disabled="!editor.can().chain().focus().toggleCode().run()"
           :class="{ 'is-active': editor.isActive('code') }"
@@ -106,12 +119,14 @@ watch(
       </div>
       <div class="history flex items-center justify-center gap-2">
         <button
+          type="button"
           @click="editor.chain().focus().undo().run()"
           :disabled="!editor.can().chain().focus().undo().run()"
         >
           <img src="~assets/images/icons/undo.png" alt="UNDO" />
         </button>
         <button
+          type="button"
           @click="editor.chain().focus().redo().run()"
           :disabled="!editor.can().chain().focus().redo().run()"
         >
@@ -124,4 +139,8 @@ watch(
 </template>
 
 <style lang="scss" scoped>
+.is-active {
+  border: 1px solid #000;
+  border-radius: 2px;
+}
 </style>
