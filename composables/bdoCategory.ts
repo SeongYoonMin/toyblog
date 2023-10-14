8e36d450a9ce6c1597d3f1f0727b721b7327d116
Seongyoon <namo4862@gmail.com>
2023-10-14 22:12:27 +0900

    ADD: composables

---
+++ composables/bdoCategory.ts
@@ -0,0 +1,15 @@
+export const blackDesertCategory = ref([
+  {
+    id: 0,
+    category_no: 1,
+    name: "Main Weapon",
+    name_kr: "주무기",
+    sub_category: [
+      {
+        id: 1,
+        name: "shield",
+        name_kr: "방패"
+      },
+    ]
+  }
+])
---
+++ composables/dfServer.ts
@@ -0,0 +1,42 @@
+export const dfServer = ref<IDfServerList[]>([
+  {
+    id: 0,
+    name: "안톤",
+    value: "anton",
+  },
+  {
+    id: 1,
+    name: "바칼",
+    value: "bakal",
+  },
+  {
+    id: 2,
+    name: "카인",
+    value: "cain",
+  },
+  {
+    id: 3,
+    name: "카시야스",
+    value: "casillas",
+  },
+  {
+    id: 4,
+    name: "디레지에",
+    value: "diregie",
+  },
+  {
+    id: 5,
+    name: "힐더",
+    value: "hilder",
+  },
+  {
+    id: 6,
+    name: "프레이",
+    value: "prey",
+  },
+  {
+    id: 7,
+    name: "시로코",
+    value: "siroco",
+  },
+])
