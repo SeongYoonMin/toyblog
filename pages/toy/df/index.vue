1340dc5e3ce999a17504d50672d22b90ea4ca5a4
Seongyoon <namo4862@gmail.com>
2023-10-14 22:12:49 +0900

    Fix: toy

--- pages/toy/df/index.vue
+++ pages/toy/df/index.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const config = useRuntimeConfig();
+const route = useRoute();
+const router = useRouter();
+
+/** DF Character는 URL Encoding 필요 */
+const searchQuery = computed(() => {
+  const { server, character } =
+    route.query;
+  return <ISearchDf>{
+    server: server?.toString() || "",
+    character: character?.toString() || ""
+  };
+})
+</script>
 
 <template>
-  <section></section>
+  <section class="df flex flex-col justify-center gap-10">
+    <picture class="header w-full flex items-center justify-center"><img src="~assets/images/icons/df.png" alt=""></picture>
+    <div class="info"></div>
+  </section>
 </template>
 
 <style lang="scss" scoped></style>
--- pages/toy/index.vue
+++ pages/toy/index.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section>
-    <NuxtLink to="toy/df">DF</NuxtLink>
+  <section class="toy grid grid-cols-4 gap-4">
+    <NuxtLink to="toy/df" class="w-full block"><img src="~assets/images/icons/df.png" alt="D&F" class="w-full"></NuxtLink>
   </section>
 </template>
 
