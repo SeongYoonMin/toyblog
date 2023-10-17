3daf4657a99f899c381a8b25aca73978ee15d33e
Seongyoon <namo4862@gmail.com>
2023-10-17 19:41:00 +0900

    FIx: add ISearchDfChar

--- types/index.d.ts
+++ types/index.d.ts
@@ -19,3 +19,16 @@ interface IDfServerList {
   name: string;
   value: string;
 }
+interface ISearchDfChar {
+  rows: { 
+    serverId: string;
+    characterId: string;
+    characterName: string;
+    level: number;
+    jobId: string;
+    jobGrowId: string;
+    jobName: string;
+    jobGrowName: string;
+    fame: number;
+  }[]
+}
