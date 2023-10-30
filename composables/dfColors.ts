712aa10a639b9e8fd0b5616e9a6c44437081bd86
SeongYoonMin <namo4862@naver.com>
2023-10-30 23:23:11 +0900

    ADD: dfColors

---
+++ composables/dfColors.ts
@@ -0,0 +1,42 @@
+export const dfColors = ref<IDfRarityColors[]>([
+  {
+    id: 0,
+    rarity: '커먼',
+    colorCode: '#FFFFFF',
+  },
+  {
+    id: 1,
+    rarity: '언커먼',
+    colorCode: '#68D5ED'
+  },
+  {
+    id: 2,
+    rarity: '레어',
+    colorCode: '#B36BFF'
+  },
+  {
+    id: 3,
+    rarity: '유니크',
+    colorCode: '#FF00FF'
+  },
+  {
+    id: 4,
+    rarity: '에픽',
+    colorCode: '#FFB400'
+  },
+  {
+    id: 5,
+    rarity: '크로니클',
+    colorCode: '#FF6666'
+  },
+  {
+    id: 6,
+    rarity: '레전더리',
+    colorCode: '#FF7800'
+  },
+  {
+    id: 7,
+    rarity: '신화',
+    colorCode: '#000000'
+  },
+])
