76c8be672cc377b6095f5d9d8ebea434b2648e56
SeongYoonMin <namo4862@naver.com>
2023-11-01 22:14:22 +0900

    ADD: main layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-
+definePageMeta({
+  layout: "main",
+});
 </script>
 
 <template>
-  <section>TEST</section>
+  <section class="w-full h-screen box-border flex items-center justify-center">
+    <picture
+      ><img src="../assets/images/main_img.png" alt="메인 배경 이미지"
+    /></picture>
+  </section>
 </template>
 
 <style lang="scss" scoped></style>
