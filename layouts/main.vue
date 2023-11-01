0a4dcd0129da1518bd80b14063f1e5e7c1c65350
SeongYoonMin <namo4862@naver.com>
2023-11-01 22:14:07 +0900

    ADD: main layout

---
+++ layouts/main.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="app">
+    <LayoutHeader />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#app {
+  background-color: #DEE7E9;
+}
+</style>
