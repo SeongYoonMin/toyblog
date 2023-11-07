e44c3f91884d427e8a019be4e5cfa8f8c9c8646a
SeongYoonMin <namo4862@naver.com>
2023-11-07 23:46:04 +0900

    Fix: Nuxtlink

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -1,63 +1,80 @@
 <script setup lang="ts">
-const route = useRoute();
-const nowPath = ref<string>(route.path);
-watch(() => route.path, (path: string) => {
-  nowPath.value = path;
-});
+const links = ref<{ to: string; title: string }[]>([
+  {
+    to: "/about",
+    title: "ABOUT",
+  },
+  {
+    to: "/board",
+    title: "BOARD",
+  },
+  {
+    to: "/toy",
+    title: "TOY",
+  },
+  {
+    to: "/contact",
+    title: "CONTACT",
+  },
+]);
+const blanks = ref<{ to: string; src: string; title: string }[]>([
+  {
+    to: "https://velog.io/@villain_uni",
+    src: "blank/new_velog.svg",
+    title: "VELOG",
+  },
+  {
+    to: "https://github.com/SeongYoonMin",
+    src: "blank/new_github.svg",
+    title: "GITHUB",
+  },
+  {
+    to: "mailto:namo4862@gmail.com",
+    src: "blank/new_google.svg",
+    title: "GOOGLE",
+  },
+]);
 </script>
 
 <template>
   <header
     id="header"
-    class="flex items-center justify-between sticky w-full top-0 left-0 right-0 z-10 px-4 py-2"
-    :class="{ main_header : nowPath === '/'}"
+    class="flex items-center justify-between sticky w-full top-0 left-0 right-0 z-10 h-[74px] mx-auto max-w-[1296px]"
   >
-    <NuxtLink to="/" class="link">HOME</NuxtLink>
-    <ul class="flex items-center justify-center gap-3">
-      <li><NuxtLink to="/about" class="link">ABOUT</NuxtLink></li>
-      <li><NuxtLink to="/board" class="link">BOARD</NuxtLink></li>
-      <li><NuxtLink to="/toy" class="link">TOY</NuxtLink></li>
-      <li><NuxtLink to="/contact" class="link">CONTACT</NuxtLink></li>
-    </ul>
-    <div class="flex items-center justify-center gap-3">
+    <NuxtLink to="/" class="link">
+      <picture
+        ><img src="../assets/images/icons/home.svg" alt="홈버튼"
+      /></picture>
+    </NuxtLink>
+    <nav class="flex items-center justify-center">
       <NuxtLink
-        class="max-w-[24px] hover:scale-125 duration-300"
-        target="_blank"
-        to="https://velog.io/@villain_uni"
-        ><img src="~assets/images/icons/velog.svg" alt="VELOG" class="w-[24px]"
-      /></NuxtLink>
+        v-for="(items, index) in links"
+        :key="index + 'links'"
+        class="link"
+        :to="items.to"
+        >{{ items.title }}</NuxtLink
+      >
+    </nav>
+    <div class="flex items-center justify-center">
       <NuxtLink
-        class="max-w-[24px] hover:scale-125 duration-300"
+        v-for="(items, index) in blanks"
         target="_blank"
-        to="https://github.com/SeongYoonMin"
-        ><img
-          src="~assets/images/icons/github.svg"
-          alt="GITHUB"
-          class="w-[24px]"
-      /></NuxtLink>
-      <NuxtLink
-        class="max-w-[24px] hover:scale-125 duration-300"
-        target="_blank"
-        to="mailto:namo4862@gmail.com"
-        ><img
-          src="~assets/images/icons/google.svg"
-          alt="GOOGLE"
-          class="w-[24px]"
-      /></NuxtLink>
+        :key="index + 'blanks'"
+        :to="items.to"
+        class="link"
+      >
+        <img :src="items.src" :alt="items.title" />
+      </NuxtLink>
     </div>
   </header>
 </template>
 
 <style lang="scss" scoped>
 .link {
-  font-size: 24px;
-  transition: 0.3s all;
+  @apply text-white text-2xl px-5 duration-300;
   &:hover {
-    color: #87ceeb;
-  }
-}
-.main_header {
-  background-color: #DEE7E9;
-  position: absolute;
+    color: #00ff00;
+    text-shadow: 1px 4px 5px rgba(0, 255, 0, 0.5);
+  };
 }
 </style>
