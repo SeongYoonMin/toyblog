7a2f3449b7b866a9fab96519801c995f06265a08
SeongYoonMin <namo4862@naver.com>
2023-11-07 23:45:37 +0900

    Fix: main

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,6 @@ definePageMeta({
 
 <template>
   <section class="w-full h-screen box-border flex items-center justify-center">
-    <picture
-      ><img src="../assets/images/main_img.png" alt="메인 배경 이미지"
-    /></picture>
   </section>
 </template>
 
