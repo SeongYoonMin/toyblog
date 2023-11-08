1fcc5376339a5f4ccdd3c23e3ef8a44c518ecf31
SeongYoonMin <namo4862@naver.com>
2023-11-08 23:07:25 +0900

    ADD: ToyCard Components

---
+++ components/ToyCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  title: string; // 제목
+  date: string; // 제작일
+  img: string;
+  intro: string; // 소개링크
+  to: string; // 링크
+}
+interface Emits {
+  (e: "update:intro", value: string): void;
+  (e: "update:to", value: string): void;
+}
+const props = defineProps<Props>();
+const emits = defineEmits<Emits>();
+</script>
+
+<template>
+  <section></section>
+</template>
+
+<style lang="scss" scoped></style>
