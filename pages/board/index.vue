cf473d6557de74fc1e7ffcc7b4dc190615f64d3c
SeongYoonMin <namo4862@naver.com>
2023-11-08 23:08:12 +0900

    Fix: board design

--- pages/board/index.vue
+++ pages/board/index.vue
@@ -5,26 +5,45 @@ const { data } = await useApiFetch<IBoardItems[]>("/api/board/list", {
 </script>
 
 <template>
-  <section>
-    <div class="flex w-full items-center justify-between">
-      <h1>BOARD</h1>
-      <NuxtLink to="/board/write">WRITE</NuxtLink>
+  <section class="board pt-20 py-[120px]">
+    <div class="flex w-full items-end justify-between pb-5">
+      <h1 class="text-[88px]">BOARD</h1>
+      <NuxtLink
+        to="/board/write"
+        class="px-5 py-3 rounded-xl"
+        style="background-color: rgba(255, 255, 255, 0.16)"
+        >글쓰기</NuxtLink
+      >
     </div>
-    <div
-      v-if="data"
-      class="list flex items-start justify-start gap-2 flex-col py-10"
-    >
+    <div class="sort pb-10 flex items-center justify-end py-3">
+      <button type="button" class="px-3 text-[#9A9CA1] relative desc">최신순</button>
+      <button type="button" class="px-3 text-[#9A9CA1] asc">오래된순</button>
+    </div>
+    <div v-if="data" class="list flex items-start justify-start flex-col">
       <NuxtLink
         v-for="items in data"
         :key="items.id"
         :to="'/board/' + items.id"
-        class="items flex w-full box-border border-b-[1px] border-solid items-center border-[#c7c7c7] text-[#c7c7c7] justify-between px-2 py-1 hover:text-[#000] hover:border-[#000] duration-300"
+        class="p-5 flex flex-col items-strat justify-center border-b border-solid border-[#424242] w-full"
       >
-        <p>{{ items.title }}</p>
-        <p>{{ items.date }}</p>
+        <h2 class="text-2xl">{{ items.title }}</h2>
+        <p v-html="items.content" class="text-[#9A9CA1] pt-2 pb-3"></p>
+        <p class="text-xs text-[#4F535E]">{{ items.date }}</p>
       </NuxtLink>
     </div>
   </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.desc {
+  &::after {
+    content: "";
+    position: absolute;
+    width: 1px;
+    height: 16px;
+    background-color: #424242;
+    top: 4px;
+    right: 0px;
+  }
+}
+</style>
