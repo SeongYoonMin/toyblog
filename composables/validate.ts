c3174348ad084ec985c26b411bce2e233f36cb6f
SeongYoonMin <namo4862@naver.com>
2023-11-13 21:48:12 +0900

    ADD: validation

---
+++ composables/validate.ts
@@ -0,0 +1,6 @@
+export const validate = (
+  values: { validateName: string; validateValue: string | number }[]
+) => {
+  const status = ref<boolean>(true);
+  const problemValues = ref<string>("");
+};
