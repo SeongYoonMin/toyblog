4a35b5e6952a37d8eb6e4cac14c6a5768912a66b
SeongYoonMin <namo4862@naver.com>
2023-11-13 21:46:45 +0900

    ADD: input components

---
+++ components/custom/InputRadio.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+interface Props {
+  modelValue: string;
+  name: string;
+}
+interface Emits {
+  (e: "update:modelValue", value: string): void;
+}
+const props = defineProps<Props>();
+const emits = defineEmits<Emits>();
+const value = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value: string) {
+    emits("update:modelValue", value);
+  },
+});
+</script>
+
+<template>
+  <label :for="props.name">
+    <p class="radio_items">{{ props.name }}</p>
+    <input type="radio" :id="props.name" v-model="value" :value="props.name" class="sr-only" />
+  </label>
+</template>
+<!-- 
+  <label
+              :for="'radio' + items.id"
+              v-for="items in contactRadio"
+              :key="items.id"
+            >
+              <p class="radio_items">{{ items.value }}</p>
+              <input
+                class="sr-only"
+                type="radio"
+                :id="'radio' + items.id"
+                :value="items.value"
+                name="contact"
+                v-model="contactType"
+              />
+            </label>
+-->
+
+<style lang="scss" scoped>
+label {
+  @apply flex-1;
+  .radio_items {
+    @apply bg-[#2B2F3B] text-center rounded-xl cursor-pointer duration-300 border border-solid border-[#2B2F3B] box-border py-5;
+    &:hover {
+      @apply bg-[#020715] border-[#00EF00] text-[#00EF00] font-semibold;
+    }
+  }
+  input:checked + .radio_items {
+    @apply bg-[#020715] border-[#00EF00] text-[#00EF00] font-semibold;
+  }
+}
+</style>
---
+++ components/custom/InputText.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+interface Props {
+  type: string;
+  title: string;
+  placeholder: string;
+  modelValue: string;
+}
+interface Emits {
+  (e: "update:modelValue", value: string): void;
+}
+const props = defineProps<Props>();
+const emits = defineEmits<Emits>();
+const textValue = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(setValue: string) {
+    emits("update:modelValue", setValue);
+  }
+})
+</script>
+
+<template>
+  <label :for="props.title" class="items">
+    <p>{{ props.title }}</p>
+    <input :type="props.type" v-model="textValue" :placeholder="props.placeholder" />
+  </label>
+</template>
+
+<style lang="scss" scoped>
+.items {
+  @apply w-full flex flex-col box-border p-5 gap-y-2 border border-solid border-[#676A73] rounded-xl bg-[#2B2F3B] duration-300;
+  input {
+    @apply bg-transparent;
+  }
+  &:focus {
+    @apply border-[#00EF00] bg-[#020715];
+  }
+}
+</style>
