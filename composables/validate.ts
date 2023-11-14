53f6497038228ce085090b742ad786e4c3d0fef6
SeongYoonMin <namo4862@naver.com>
2023-11-14 22:21:49 +0900

    Fix: validation function

--- composables/validate.ts
+++ composables/validate.ts
@@ -3,4 +3,15 @@ export const validate = (
 ) => {
   const status = ref<boolean>(true);
   const problemValues = ref<string>("");
+  for(let i = 0; i < values.length; i++) {
+    const vaildateLength = values[i].validateValue.toString().length;
+    if(vaildateLength < 1) {
+      status.value = false;
+      problemValues.value = values[i].validateName.toString();
+    }
+  }
+  return {
+    status: status.value,
+    problem: problemValues.value
+  }
 };
