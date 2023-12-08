22175e8e3b02c255a83f06a289c2e8235847881b
Seongyoon <namo4862@gmail.com>
2023-12-08 15:47:21 +0900

    Fix: typedText
    typedText의 문구 변경

--- pages/index.vue
+++ pages/index.vue
@@ -6,16 +6,13 @@ const { $Typed } = useNuxtApp();
 const typedContent = ref();
 const typedText = ref<string[]>([
   "Hello, World!",
-  "안녕하세요! 저는",
-  "함께하고 싶은 개발자를 꿈꾸는",
-  "프론트엔드 주니어 개발자 민성윤입니다!",
+  "I'm Front-end Junior Developer!",
 ]);
 onMounted(() => {
   const typed = new $Typed(typedContent.value, {
     strings: typedText.value,
     typeSpeed: 150,
     backSpeed: 50,
-    
   });
 });
 </script>
@@ -30,6 +27,7 @@ onMounted(() => {
 .typed {
   @apply text-[66px];
   // text-shadow: 0 0 10px rgba(0, 255, 0, 0.5), 0 0 20px rgba(0, 255, 0, 0.5), 0 0 40px rgba(0, 255, 0, 0.5), 0 0 80px rgba(0, 255, 0, 0.5);
-  text-shadow: 0 0 10px rgb(12, 42, 126), 0 0 20px rgb(12, 42, 126), 0 0 40px rgb(12, 42, 126), 0 0 80px rgb(12, 42, 126);
+  text-shadow: 0 0 10px rgb(12, 42, 126), 0 0 20px rgb(12, 42, 126),
+    0 0 40px rgb(12, 42, 126), 0 0 80px rgb(12, 42, 126);
 }
 </style>
